dfb65d190fde71d89fb0a9a0c5db376cf9892e48
olusprogr <olus.main333@gmail.com>
2024-08-27 09:11:38 +0200

    awdad

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 443;
+const port = 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const bcrypt = require('bcrypt');
 const { OpenAI } = require("openai");
